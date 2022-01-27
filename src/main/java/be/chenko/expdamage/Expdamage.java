4dcd430302d7a66a4c0188c8592b65b465a6cf96
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-01-27 11:59:08 +0100

    added extra config options

--- src/main/java/be/chenko/expdamage/Expdamage.java
+++ src/main/java/be/chenko/expdamage/Expdamage.java
@@ -1,13 +1,15 @@
 package be.chenko.expdamage;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 public final class Expdamage extends JavaPlugin {
-    public final String pluginName = "Exp Damage";
+    private static Expdamage plugin;
+    public final String PLUGIN_NAME = "Exp Damage";
     public final Logger LOGGER = getLogger();
-
+    //FileConfiguration config = this.getConfig();
 
     @Override
     public void onEnable() {
@@ -15,21 +17,26 @@ public final class Expdamage extends JavaPlugin {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
-
+        plugin = this;
         boolean isEnabled = getConfig().getBoolean("enable-exp-damage");
 
-        LOGGER.info(String.format("[%s] has loaded", pluginName));
-        LOGGER.info(String.format("[%s] has been set to %b in the config.yml", pluginName, isEnabled));
-        if (isEnabled){
-            getServer().getPluginManager().registerEvents(new Listeners(), this);
-        }
+        LOGGER.info(String.format("[%s] has loaded", PLUGIN_NAME));
+        LOGGER.info(String.format("[%s] has been set to %b in the config.yml", PLUGIN_NAME, isEnabled));
+        LOGGER.warning(String.format("[%s] if you still have the config from version 1.2, please remove the config file.", PLUGIN_NAME));
+        getServer().getPluginManager().registerEvents(new Listeners(), this);
+
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        LOGGER.info(String.format("[%s] has unloaded", pluginName));
+        LOGGER.info(String.format("[%s] has unloaded", PLUGIN_NAME));
+        LOGGER.warning(String.format("[%s] if you still have the config from version 1.2, please remove the config file.", PLUGIN_NAME));
 
+    }
+
+    public static Expdamage getPlugin() {
+        return plugin;
     }
 }
--- src/main/java/be/chenko/expdamage/Listeners.java
+++ src/main/java/be/chenko/expdamage/Listeners.java
@@ -6,19 +6,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
 public class Listeners implements Listener {
-
+    boolean isEnabled = Expdamage.getPlugin().getConfig().getBoolean("enable-exp-damage");
+    double divider = Expdamage.getPlugin().getConfig().getDouble("amount-divider");
 
     @EventHandler
     public void PlayerExpChangeEvent(PlayerExpChangeEvent event) {
-
-
-        Player player = event.getPlayer();
-        if (!player.hasPermission("expdamage.exempt")) {
-            double dmg = event.getAmount() / 3;
-            player.damage(dmg);
+        if (isEnabled){
+            Player player = event.getPlayer();
+            if (!player.hasPermission("expdamage.exempt")) {
+                double dmg = event.getAmount() / divider;
+                player.damage(dmg);
+            }
         }
-
-
     }
 
 
